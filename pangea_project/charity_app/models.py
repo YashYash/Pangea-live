fb6cf74e47f652431c5f337392e64b34653324b1
YashYash <yash.saxena1217@gmail.com>
2014-06-29 16:31:22 -0700

    created form for charities

--- pangea_project/charity_app/models.py
+++ pangea_project/charity_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Charity(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    posted = models.DateTimeField(auto_now=True)
+    charity_url = models.CharField(max_length=1000, null=True, blank=True)
+    slogan = models.CharField(max_length=200, null=True, blank=True)
+    statement = models.CharField(max_length=1000, null=True, blank=True)
+    description = models.CharField(max_length=6000, null=True, blank=True)
+    cover_photo = models.ImageField(upload_to="images/charity_coverphoto", null=True, blank=True)
+    background_image = models.ImageField(upload_to="images/charity_background", null=True, blank=True)
+    image = models.ImageField(upload_to="images/charity_logo", null=True, blank=True)
+    video = models.URLField()
+
+    def __unicode__(self):
+        return self.name
+
