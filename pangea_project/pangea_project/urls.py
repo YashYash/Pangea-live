cdc5010e8ae9ebd7af42d1c6c1f7e035478fee01
YashYash <yash.saxena1217@gmail.com>
2014-06-29 17:38:30 -0700

    global settings

--- pangea_project/pangea_project/urls.py
+++ pangea_project/pangea_project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'user_app.views.landing', name='home'),
+    url(r'^$', 'user_app.views.landing', name='landing'),
     url(r'^giver/$', 'giver_app.views.giver_home', name='giver_home'),
     url(r'^charity/$', 'charity_app.views.charity_home', name='charity_home'),
 )
